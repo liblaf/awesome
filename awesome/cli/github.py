b77c631e214f150d9474cf73758f47bb4a0c75a3
liblaf <30631553+liblaf@users.noreply.github.com>
2024-01-17 17:37:41 +0800

    feat: add rate limit handling for GitHub API requests
    Add rate limit handling for GitHub API requests to prevent exceeding the rate limit. When the rate limit is exceeded, a warning is logged, and the program waits until the rate
    limit is reset before retrying the request. This prevents errors due to rate limit exceedance and ensures smooth functioning of the application.

--- awesome/cli/github.py
+++ awesome/cli/github.py
@@ -1,5 +1,5 @@
 import asyncio
-import math
+import datetime
 import os
 import pathlib
 from collections.abc import Mapping, Sequence
@@ -17,12 +17,33 @@ from tenacity import stop, wait
 
 
 class Repository(pydantic.BaseModel):
-    commits: int = math.nan
+    commits: int = -1
     description: Optional[str]
     full_name: str
     html_url: str
     name: str
     stargazers_count: int
+
+
+class RateLimit(pydantic.BaseModel):
+    class Rate(pydantic.BaseModel):
+        limit: int
+        used: int
+        remaining: int
+        reset: datetime.datetime
+
+    rate: Rate
+
+
+async def handle_rate_limit(client: httpx.AsyncClient) -> None:
+    loguru.logger.warning("Rate Limit Exceeded")
+    response: httpx.Response = await client.get("https://api.github.com/rate_limit")
+    response.raise_for_status()
+    rate_limit: RateLimit = RateLimit(**response.json())
+    await asyncio.sleep(
+        rate_limit.rate.reset.timestamp() - datetime.datetime.now().timestamp()
+    )
+    raise tenacity.TryAgain()
 
 
 @tenacity.retry(stop=stop.stop_after_attempt(4), wait=wait.wait_random_exponential())
@@ -31,18 +52,23 @@ async def get_commits(repo: str, token: Optional[str] = None) -> int:
         headers={"Authorization": f"Bearer {token}"} if token else None,
         follow_redirects=True,
     ) as client:
-        response: httpx.Response = await client.get(
-            f"https://api.github.com/repos/{repo}/stats/commit_activity"
-        )
-        response.raise_for_status()
-        if response.status_code == 202:
-            raise tenacity.TryAgain()
-        return sum(week["total"] for week in response.json())
+        for attempt in tenacity.AsyncRetrying(
+            stop=stop.stop_after_attempt(32), wait=wait.wait_random_exponential()
+        ):
+            with attempt:
+                response: httpx.Response = await client.get(
+                    f"https://api.github.com/repos/{repo}/stats/commit_activity"
+                )
+                if response.status_code == 403:
+                    await handle_rate_limit(client=client)
+                response.raise_for_status()
+                if response.status_code == 202:
+                    raise tenacity.TryAgain()
+                return sum(week["total"] for week in response.json())
+        raise RuntimeError("Unreachable")
 
 
-@tenacity.retry(
-    stop=stop.stop_after_attempt(32), wait=wait.wait_random_exponential(min=1)
-)
+@tenacity.retry(stop=stop.stop_after_attempt(4), wait=wait.wait_random_exponential())
 async def get_repo(repo: str, token: Optional[str] = None) -> Repository:
     async with httpx.AsyncClient(
         headers={"Authorization": f"Bearer {token}"} if token else None,
@@ -51,6 +77,8 @@ async def get_repo(repo: str, token: Optional[str] = None) -> Repository:
         response: httpx.Response = await client.get(
             f"https://api.github.com/repos/{repo}"
         )
+        if response.status_code == 403:
+            await handle_rate_limit(client=client)
         response.raise_for_status()
         result: Repository = Repository(
             **response.json(), commits=await get_commits(repo=repo, token=token)
