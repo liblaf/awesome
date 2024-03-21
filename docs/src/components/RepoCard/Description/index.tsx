8687c9af8e8702281b90cc97300ebaec3fdc6d9f
liblaf <30631553+liblaf@users.noreply.github.com>
2024-03-21 22:15:35 +0800

    fix: update RepoCard Description component to render Markdown
    Integrate Markdown rendering in the Description component to support enhanced text formatting and emoji support.

--- docs/src/components/RepoCard/Description/index.tsx
+++ docs/src/components/RepoCard/Description/index.tsx
@@ -1,9 +1,19 @@
+import Markdown from "react-markdown";
+import remarkGemoji from "remark-gemoji";
+import remarkGfm from "remark-gfm";
 import styles from "./styles.module.css";
 
 export default function Description({
   children,
 }: {
-  children: React.ReactNode;
+  children?: string;
 }): JSX.Element {
-  return <p className={styles.description}>{children}</p>;
+  return (
+    <Markdown
+      className={styles.description}
+      remarkPlugins={[remarkGemoji, remarkGfm]}
+    >
+      {children}
+    </Markdown>
+  );
 }
