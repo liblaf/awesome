263a5336465d2c04c324803966351c545a1aeae1
liblaf <30631553+liblaf@users.noreply.github.com>
2024-03-21 18:24:15 +0800

    fix(data, components): add WebsiteCard component to display website info
    Introduce WebsiteCard component to render website details in AwesomeList component, enhancing the display of website information. This change improves the user experience by including website descriptions, favicons, and images alongside repository details.

--- docs/src/components/AwesomeList/index.tsx
+++ docs/src/components/AwesomeList/index.tsx
@@ -2,9 +2,12 @@ import DATA from "@site/data/awesome.json";
 import type { Repo } from "@site/src/components/RepoCard";
 import RepoCard from "@site/src/components/RepoCard";
 import styles from "./styles.module.css";
+import type { Website } from "@site/src/components/WebsiteCard";
+import WebsiteCard from "@site/src/components/WebsiteCard";
 
 type Data = {
   repos: Repo[];
+  websites: Website[];
 };
 
 const DEMO: Data = {
@@ -20,16 +23,32 @@ const DEMO: Data = {
       stars: 1,
     },
   ],
+  websites: [
+    {
+      description:
+        "GitHub is where over 100 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and fea...",
+      favicon: "https://icons.bitwarden.net/github.com/icon.png",
+      image:
+        "https://github.githubassets.com/assets/campaign-social-031d6161fa10.png",
+      title: "GitHub: Let’s build from here · GitHub",
+      url: "https://github.com/",
+    },
+  ],
 };
 
 export default function AwesomeList({ name }: { name?: string }): JSX.Element {
   const data: Data = DATA.data[name] ?? DEMO;
   return (
     <div className={styles.cards}>
+      {data.websites.map(
+        (website: Website): JSX.Element => (
+          <WebsiteCard website={website} />
+        )
+      )}
       {data.repos.map(
         (repo: Repo): JSX.Element => (
           <RepoCard repo={repo} />
-        ),
+        )
       )}
     </div>
   );
---
+++ docs/src/components/WebsiteCard/index.tsx
@@ -0,0 +1,51 @@
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import Description from "./Description";
+
+export type Website = {
+  description?: string;
+  favicon: string;
+  image?: string;
+  title?: string;
+  url: string;
+};
+
+function Favicon({ src }: { src?: string }): JSX.Element {
+  if (!src) {
+    return <></>;
+  }
+  return <img className={styles.favicon} src={src} />;
+}
+
+export default function WebsiteCard({
+  website,
+}: {
+  website: Website;
+}): JSX.Element {
+  let background: React.CSSProperties = undefined;
+  if (website.image) {
+    background = {
+      background: `url(${website.image}) center center / cover no-repeat`,
+    };
+  }
+  return (
+    <Link
+      className={clsx(
+        "card",
+        styles.card,
+        website.image ? styles.image : undefined
+      )}
+      style={background}
+      to={website.url}
+    >
+      <div className={styles.detail}>
+        <div className={styles.title}>
+          <Favicon src={website.favicon} />
+          <span>{website.title ?? website.url}</span>
+        </div>
+        <Description>{website.description}</Description>
+      </div>
+    </Link>
+  );
+}
