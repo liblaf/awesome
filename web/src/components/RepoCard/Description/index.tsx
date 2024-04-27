7c5d20e0725e65b04026c4c2d5581cbee3f1dd0c
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-27 19:13:00 +0800

    fix: removed unnecessary check for empty html prop

--- web/src/components/RepoCard/Description/index.tsx
+++ web/src/components/RepoCard/Description/index.tsx
@@ -1,9 +1,6 @@
 import styles from "./styles.module.css";
 
 export default function Description({ html }: { html?: string }): JSX.Element {
-  if (!html) {
-    return <></>;
-  }
   return (
     <div
       className={styles.description}
