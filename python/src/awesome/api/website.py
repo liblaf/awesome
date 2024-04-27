e59762f5feed04035ba36b9708f9aa9385303ba6
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-27 21:09:40 +0800

    fix: improve image fetching logic in fetch_website
    Ensure that images are fetched correctly by checking the response status before processing the image URL.

--- python/src/awesome/api/website.py
+++ python/src/awesome/api/website.py
@@ -77,13 +77,9 @@ def _get_description(soup: bs4.BeautifulSoup) -> str | None:
     return None
 
 
-async def fetch_website(url: str) -> Website:
+async def fetch_website(client: httpx.AsyncClient, url: str) -> Website:
     try:
-        async with httpx.AsyncClient(
-            headers={"User-Agent": UA.chrome},  # pyright: ignore
-            follow_redirects=True,
-        ) as client:
-            response: httpx.Response = await client.get(url)
+        response: httpx.Response = await client.get(url)
         response = response.raise_for_status()
         soup: bs4.BeautifulSoup = bs4.BeautifulSoup(
             response.text, features="html.parser"
@@ -91,6 +87,10 @@ async def fetch_website(url: str) -> Website:
         image: str | None = _get_image(soup)
         if image and not image.startswith("http"):
             image = parse.urljoin(url, image)
+        if image:
+            response = await client.get(image)
+            if not response.is_success:
+                image = None
         return Website(
             url=url,  # pyright: ignore
             title=_get_title(soup),
@@ -103,4 +103,8 @@ async def fetch_website(url: str) -> Website:
 
 
 async def fetch_website_list(urls: Iterable[str]) -> list[Website]:
-    return await asyncio.gather(*[fetch_website(url) for url in urls])
+    async with httpx.AsyncClient(
+        headers={"User-Agent": UA.chrome},  # pyright: ignore
+        follow_redirects=True,
+    ) as client:
+        return await asyncio.gather(*[fetch_website(client, url) for url in urls])
